37c687d816ee9bfbd2ad8d9a077fec1e9ad10577
gokhanmeteerturk <me@e-mete.com>
2025-05-15 17:51:21 +0100

    lower resources

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,8 +1,11 @@
 import logging
 
 bind = "0.0.0.0:8000"
-workers = 4
+workers = 2
 worker_class = "uvicorn.workers.UvicornWorker"
+
+max_requests = 100
+max_requests_jitter = 10
 
 # Logging configuration
 loglevel = "info"  # Set the log level to capture errors and above
